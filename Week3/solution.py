4823ee1f28db386272c2b6ac19e83b7a3d1fdf55
Ben Wilke <benjamin.wilke@oracle.com>
2020-03-30 10:37:44 -0500

    adding week3

---
+++ Week3/solution.py
@@ -0,0 +1,9 @@
+def logtolist(log):
+    logList = list()
+    for line in log:
+        currentEntry = dict()
+        currentEntry["ip_address"] = line.split(' - - ')[0]
+        currentEntry["timestamp"] = line.split('[')[1].split(']')[0]
+        currentEntry["request"] = line.split('\"')[1]
+        logList.append(currentEntry)
+    return logList
